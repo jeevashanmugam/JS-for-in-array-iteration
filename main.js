8310a5da1a409fb90b2591af5cee8774c693a1f5
jeevashanmugam <jeevasj2000@gmail.com>
2023-06-19 13:34:43 +0530

    JS-for-in-array-iteration

---
+++ main.js
@@ -0,0 +1,27 @@
+
+
+let arr = ["shanmugam", "jaya", "shalini", "jeeva", 18, 24, 02, 03]
+
+document.write("Its returns the index : ")
+
+for (let newName in arr) {
+
+    document.write((newName) + " ");
+
+}
+
+document.write("<br> <br>")
+document.write("Its returns the element : ")
+
+//If I want element means use this //arr[newName]
+
+for (let newName in arr) {
+
+    document.write(arr[newName] + " ");
+
+}
+
+
+
+
+
